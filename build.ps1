ce3c69068b099027e88af76ff2e2b7d63f170077
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-07-09 06:25:53 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -10,21 +10,21 @@
 # npm run html $folder
 # npm run pdf $folder
 
-$folder = "public\lectures\T04 - CSS 2"
+# $folder = "public\lectures\T04 - CSS 2"
+# npm run html $folder
+# npm run pdf $folder
+
+$folder = "public\lectures\T05 - JavaScript"
 npm run html $folder
 npm run pdf $folder
 
-# $folder = "public\lectures\T05 - JavaScript"
-# npm run html $folder
-# npm run pdf $folder
+$folder = "public\lectures\T06 - TypeScript"
+npm run html $folder
+npm run pdf $folder
 
-# $folder = "public\lectures\T06 - TypeScript"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T07 - DOM"
-# npm run html $folder
-# npm run pdf $folder
+$folder = "public\lectures\T07 - DOM"
+npm run html $folder
+npm run pdf $folder
 
 # $folder = "public\lectures\T08 - React"
 # npm run html $folder
